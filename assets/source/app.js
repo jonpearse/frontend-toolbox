bb3899364c117a8bb8cd8b7aa41afc6e15980ac9
Jon Pearse <jon@jonpearse.net>
2017-03-05 19:37:42 +0000

    Properly including customevent shim

--- assets/source/app.js
+++ assets/source/app.js
@@ -2,11 +2,12 @@
 // require('vendor/scriptname');
 
 // Extend things
+require('ext/CustomEvent');
 require('ext/NodeList');
 require('ext/Number');
 require('ext/Object');
 
 // Init behaviours
-require('core/behaviours').init([
+/*require('core/behaviours').init([
     // require('behaviour/EXAMPLE')
-]);
+]);*/
